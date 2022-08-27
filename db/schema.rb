d33f2dd6130742d925b3e2a0627e1c9aa3e459e5
MeqdamAlqudah <memuq56@gmail.com>
2022-08-27 20:36:49 +0300

    add password validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_000301) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_084154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_08_000301) do
     t.text "description"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.integer "user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -34,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_08_000301) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
