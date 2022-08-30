6c20387bb0a6bce8827255baf1c6b67c4f72d49c
MeqdamAlqudah <memuq56@gmail.com>
2022-08-30 17:13:53 +0300

    the course tutor solution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
     def new 
-
-        user = User.create(username:params[:username],email:params[:email],password_digest:params[:password])
+        
+        @user = User.create(username:params[:username],email:params[:email],password_digest:params[:password])
         
     end
     def create 
-        user = User.create(username:params[:username],email:params[:email],password_digest:params[:password])
+        @user = User.create(username:params[:user]["username"],email:params[:user]["email"],password_digest:params[:user]["password"])
 
-        if(user.save)
+
+        if(@user.save)
             flash[:notice] = "User signUp  successfully."
             redirect_to articles_path
         else    
-            render json:params 
+            render :new
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-  resources :users
+  resources :users,except:[:new]
   resources :categories
   get 'signup',to: 'users#new'
+  
 end
