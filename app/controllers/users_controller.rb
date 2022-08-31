3f1624506437d7e630a850d2eaae9f1674a08eb8
MeqdamAlqudah <memuq56@gmail.com>
2022-08-31 22:38:40 +0300

    my solution for user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
         @user = User.new
         
     end
+    def show 
+        @user = User.find(params[:id])
+        @articles = Article.where(user_id:params[:id])
+    end
     def create 
         @user = User.create(user_params)
 
@@ -17,16 +21,17 @@ class UsersController < ApplicationController
 
     end
     def edit 
-        @user = User.find_by(id:params[:id])
+        @user = User.find(params[:id])
     end
     def update 
-        user = User.find_by(id:params[:id])
-        if(user.update(user_params))
-            flash[:notice] = "User #{user.username} Updated sucssessfully"
+        @user = User.find(params[:id])
+        if(@user.update(user_params))
+            flash[:notice] = "Your account information was successfully updated"
             redirect_to articles_path
         else
-            render :edit
+            render 'edit'
         end
+
     end
     def user_params 
         params.require(:user).permit(:username,:email,:password)
