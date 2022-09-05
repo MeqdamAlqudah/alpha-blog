e23a5764a999534b2a780a867bd3b907471203e3
MeqdamAlqudah <memuq56@gmail.com>
2022-09-05 18:02:13 +0300

    toutr solution for login logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,20 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    @user = User.where(email: params[:email])[0]
-    if @user.authenticate(params[:password])
-      redirect_to user_path(@user.id)
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully'
+      redirect_to user_path(user)
     else
+      flash.now[:alert] = 'There was something wrong with your login detail'
       render 'new'
     end
   end
 
-  def destroy; end
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out successfully'
+    redirect_to root_path
+  end
 end
