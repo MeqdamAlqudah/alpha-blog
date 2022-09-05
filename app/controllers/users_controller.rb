2d8ee9412b1e00463cb4f31d3453379c89227331
MeqdamAlqudah <memuq56@gmail.com>
2022-09-05 14:45:45 +0300

    My solution to user login tech

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,43 @@
 class UsersController < ApplicationController
-    def index 
-        @users = User.all
-    end
-    
-    def new 
-        
-        @user = User.new
-        
-    end
-    def show 
-        @user = User.find(params[:id])
-        @articles = Article.where(user_id:params[:id])
-    end
-    def create 
-        @user = User.create(user_params)
+  def index
+    @users = User.all
+  end
 
+  def new
+    @user = User.new
+  end
 
-        if(@user.save)
-            flash[:notice] = "Welcome to alpha blog #{@user.username}, you have signUp  successfully."
-            redirect_to articles_path
-        else    
-            render :new
-        end
+  def show
+    @user = User.find(params[:id])
+    @articles = Article.where(user_id: params[:id])
+  end
 
-    end
-    def edit 
-        @user = User.find(params[:id])
-    end
-    def update 
-        @user = User.find(params[:id])
-        if(@user.update(user_params))
-            flash[:notice] = "Your account information was successfully updated"
-            redirect_to articles_path
-        else
-            render 'edit'
-        end
+  def create
+    @user = User.create(user_params)
 
+    if @user.save
+      flash[:notice] = "Welcome to alpha blog #{@user.username}, you have signUp  successfully."
+      redirect_to user_path(@user)
+    else
+      render :new
     end
-    def user_params 
-        params.require(:user).permit(:username,:email,:password)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Your account information was successfully updated'
+      redirect_to user_path(@user)
+    else
+      render 'edit'
     end
-    
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
