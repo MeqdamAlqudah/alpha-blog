c0bca7d5582aeb236752ab0e3b826e6417accf68
MeqdamAlqudah <memuq56@gmail.com>
2022-09-13 12:31:50 +0300

    increase number of words for the article describtion

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,6 @@ class Article < ApplicationRecord
   has_many :article_categories
   has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 900 }
   self.per_page = 10
 end
