4f41b5a735ad07388a9ee63ca3451684a7fb4b30
MeqdamAlqudah <memuq56@gmail.com>
2022-09-13 12:45:51 +0300

    Fix user edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = 'You can only edit or delete your own account!!'
       redirect_to @user
     end
